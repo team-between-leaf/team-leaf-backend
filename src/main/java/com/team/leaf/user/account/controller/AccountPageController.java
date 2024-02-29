d2a625ded1c74982ef862ca947ceb6a54bf0c895
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2024-03-01 02:11:25 +0900

    feat: access token 갱신 기능 추가 (#14)

---
+++ src/main/java/com/team/leaf/user/account/controller/AccountPageController.java
@@ -0,0 +1,36 @@
+package com.team.leaf.user.account.controller;
+
+import com.team.leaf.user.account.dto.request.UpdateAccountDto;
+import com.team.leaf.user.account.dto.response.AccountDto;
+import com.team.leaf.user.account.exception.ApiResponse;
+import com.team.leaf.user.account.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/account/mypage")
+public class AccountPageController {
+
+    private final AccountService accountService;
+
+    //mypage 파트 수정 예정....!
+
+    /*//유저 정보 조회
+    @GetMapping("/{email}/search")
+    public ApiResponse<AccountDto> getAccount(@PathVariable String email) {
+        AccountDto accountDto = accountService.getAccount(email);
+
+        return new ApiResponse<>(accountDto);
+    }
+
+    //유저 정보 수정
+    @PutMapping("/{email}/update")
+    public ApiResponse<String> updateAccount(@PathVariable String email, UpdateAccountDto accountDto) throws IOException {
+        return new ApiResponse<>(accountService.updateUser(email,accountDto));
+    }*/
+
+}
+
