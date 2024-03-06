27ffb81262cbee6dad61084b329e90ea5b677fc9
hershyYJ <juj0478@naver.com>
2024-03-06 19:46:14 +0900

    fix: 중복 회원 조회 api 수정

--- src/main/java/com/team/leaf/user/account/controller/AccountController.java
+++ src/main/java/com/team/leaf/user/account/controller/AccountController.java
@@ -1,23 +1,21 @@
 package com.team.leaf.user.account.controller;
 
-import com.team.leaf.user.account.dto.request.JoinRequest;
-import com.team.leaf.user.account.dto.request.LoginRequest;
+import com.team.leaf.user.account.dto.request.jwt.JwtJoinRequest;
+import com.team.leaf.user.account.dto.request.jwt.JwtLoginRequest;
 import com.team.leaf.user.account.dto.response.AccountDto;
-import com.team.leaf.user.account.dto.response.GlobalResDto;
 import com.team.leaf.user.account.dto.response.LoginAccountDto;
 import com.team.leaf.user.account.dto.response.TokenDto;
 import com.team.leaf.user.account.exception.ApiResponse;
 import com.team.leaf.user.account.jwt.JwtTokenUtil;
-import com.team.leaf.user.account.jwt.PrincipalDetails;
 import com.team.leaf.user.account.service.AccountService;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import com.team.leaf.user.account.service.CommonService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -28,30 +26,29 @@ import java.io.IOException;
 public class AccountController {
 
     private final AccountService accountService;
-    private final JwtTokenUtil jwtTokenUtil;
+    private final CommonService commonService;
 
     @PostMapping("/join")
-    public ApiResponse<String> join(@RequestBody JoinRequest joinRequest) throws IOException {
+    @Operation(summary= "자체 로그인 회원가입 API")
+    public ApiResponse<String> join(@RequestBody JwtJoinRequest joinRequest) throws IOException {
 
         return new ApiResponse<>(accountService.join(joinRequest));
     }
 
-    @GetMapping(value = "/join/{phone}")
-    public ResponseEntity<Boolean> phoneCheck(@PathVariable String phone) {
-        return ResponseEntity.ok(accountService.checkPhoneDuplicate(phone));
-    }
-
     @PostMapping("/login")
-    public ApiResponse<LoginAccountDto> login(@RequestBody @Valid LoginRequest loginRequest, HttpServletResponse response) {
+    @Operation(summary= "자체 로그인 로그인 API")
+    public ApiResponse<LoginAccountDto> login(@RequestBody @Valid JwtLoginRequest loginRequest, HttpServletResponse response) {
         return new ApiResponse<>(accountService.login(loginRequest, response));
     }
 
     @DeleteMapping("/logout/{userEmail}")
+    @Operation(summary= "자체 로그인 로그아웃 API")
     public ApiResponse<String> logout(@PathVariable String userEmail) {
         return new ApiResponse<>(accountService.logout(userEmail));
     }
 
     @PostMapping("/issue/token")
+    @Operation(summary= "Access Token 갱신 API")
     public ResponseEntity<?> refreshAccessToken(@RequestHeader(name = JwtTokenUtil.REFRESH_TOKEN, required = false) String refreshToken) {
         try {
             TokenDto newTokenDto = accountService.refreshAccessToken(refreshToken);
@@ -67,10 +64,23 @@ public class AccountController {
     }
 
     @GetMapping("/{userEmail}")
+    @Operation(summary= "중복 이메일 확인 API")
     public ResponseEntity findAccountById(@PathVariable String userEmail) {
         AccountDto account = accountService.getAccount(userEmail);
 
         return ResponseEntity.ok(account);
+    }
+
+    @PostMapping("/check/{phone}")
+    @Operation(summary= "중복 회원 확인 API")
+    public ResponseEntity<String> checkPhoneDuplicate(@PathVariable String phone) {
+        String existingUserMessage = commonService.checkPhoneNumberDuplicate(phone);
+
+        if (!"중복된 데이터가 없습니다.".equals(existingUserMessage)) {
+            return ResponseEntity.badRequest().body(existingUserMessage);
+        }
+
+        return ResponseEntity.ok("중복된 데이터가 없습니다.");
     }
 
 }
