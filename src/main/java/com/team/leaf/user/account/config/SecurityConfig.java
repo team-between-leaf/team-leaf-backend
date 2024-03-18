07ca187b70db6502e498c9f0423613b510b1f217
hershyYJ <juj0478@naver.com>
2024-03-18 17:25:07 +0900

    feat: AuthenticationManager 빈 등록

--- src/main/java/com/team/leaf/user/account/config/SecurityConfig.java
+++ src/main/java/com/team/leaf/user/account/config/SecurityConfig.java
@@ -1,35 +1,54 @@
 package com.team.leaf.user.account.config;
 
+import com.team.leaf.user.account.entity.AccountRole;
 import com.team.leaf.user.account.jwt.JwtTokenFilter;
 import com.team.leaf.user.account.jwt.JwtTokenUtil;
 import com.team.leaf.user.account.jwt.PrincipalDetailsService;
-import com.team.leaf.user.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityConfigurer;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
-public class JwtSecurityConfig {
+public class SecurityConfig {
 
     private final JwtTokenUtil jwtTokenUtil;
+    private final PrincipalDetailsService principalDetailsService;
+    private final RedisTemplate redisTemplate;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(principalDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     @Bean
@@ -43,11 +62,17 @@ public class JwtSecurityConfig {
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers("/**" , "/account/**" , "/product/**" , "/alert/**" , "/cart/**" , "/seller/**").permitAll()
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        /*.requestMatchers("/alert/notify").hasAnyRole(AccountRole.USER.name(), AccountRole.SELLER.name())
+                        .requestMatchers("/cart/**", "/chat/room/buyer", "/product/{productId}/coupon/download/{couponId}", "/follow", "/product/{productId}/wish-list").hasRole(AccountRole.USER.name())
+                        .requestMatchers(HttpMethod.GET, "/product/wish").hasRole(AccountRole.USER.name())
+                        .requestMatchers("/chat/room/seller", "/product/{productId}/coupon", "/seller/notice", "/seller/notice/{noticeId}").hasRole(AccountRole.SELLER.name())
+                        .requestMatchers("/account/**", "/alert/**", "/board/**", "/chat/**", "/product/**", "/seller/**", "/error/**").permitAll()*/
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(new JwtTokenFilter(jwtTokenUtil), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(new JwtTokenFilter(jwtTokenUtil, redisTemplate), UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
 
 }
+
