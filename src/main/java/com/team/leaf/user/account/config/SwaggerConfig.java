0903648c22772fc7317eafa5bd6f186bc55f19ce
hershyYJ <juj0478@naver.com>
2024-03-18 17:26:29 +0900

    fix: swagger 설정 변경

--- src/main/java/com/team/leaf/user/account/config/SwaggerConfig.java
+++ src/main/java/com/team/leaf/user/account/config/SwaggerConfig.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
@@ -25,19 +24,17 @@ public class SwaggerConfig {
 
         // Security 스키마 설정
         SecurityScheme bearerAuth = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
+                .type(SecurityScheme.Type.APIKEY)
                 .in(SecurityScheme.In.HEADER)
-                .name(HttpHeaders.AUTHORIZATION);
+                .name("Authorization");
 
         // Security 요청 설정
         SecurityRequirement addSecurityItem = new SecurityRequirement();
-        addSecurityItem.addList("JWT");
+        addSecurityItem.addList("Bearer Token");
 
         return new OpenAPI()
                 // Security 인증 컴포넌트 설정
-                .components(new Components().addSecuritySchemes("JWT", bearerAuth))
+                .components(new Components().addSecuritySchemes("Bearer Token", bearerAuth))
                 // API 마다 Security 인증 컴포넌트 설정
                 .addSecurityItem(addSecurityItem)
                 .info(info);
