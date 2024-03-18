a77461c31aee36f51a1acafd417e29afa3497330
hershyYJ <juj0478@naver.com>
2024-03-18 20:08:41 +0900

    feat: 소셜 로그인 유저 등록 코드 추가

--- src/main/java/com/team/leaf/user/account/jwt/PrincipalDetails.java
+++ src/main/java/com/team/leaf/user/account/jwt/PrincipalDetails.java
@@ -1,6 +1,7 @@
 package com.team.leaf.user.account.jwt;
 
 import com.team.leaf.user.account.entity.AccountDetail;
+import com.team.leaf.user.account.entity.OAuth2Account;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -10,6 +11,7 @@ import java.util.Collection;
 public class PrincipalDetails implements UserDetails {
 
     private AccountDetail accountDetail;
+    private OAuth2Account oAuth2Account;
 
     public void setAccountDetail(AccountDetail accountDetail) {
         this.accountDetail = accountDetail;
@@ -19,24 +21,46 @@ public class PrincipalDetails implements UserDetails {
         return this.accountDetail;
     }
 
+    public void setOAuth2Account(OAuth2Account oAuth2Account) {
+        this.oAuth2Account = oAuth2Account;
+    }
+
+    public OAuth2Account getOAuth2Account() {
+        return this.oAuth2Account;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> collections = new ArrayList<>();
-        collections.add(() -> {
-            return accountDetail.getRole().name();
-        });
+        if(accountDetail != null) {
+            collections.add(() -> {
+                return accountDetail.getRole().name();
+            });
+        } else if(oAuth2Account != null) {
+            collections.add(() -> {
+                return oAuth2Account.getRole().name();
+            });
+        }
 
         return collections;
     }
 
     @Override
     public String getPassword() {
-        return accountDetail.getPassword();
+        if(accountDetail != null) {
+            return accountDetail.getPassword();
+        }
+        return null;
     }
 
     @Override
     public String getUsername() {
-        return accountDetail.getEmail();
+        if(accountDetail != null) {
+            return accountDetail.getEmail();
+        } else if(oAuth2Account != null) {
+            return oAuth2Account.getEmail();
+        }
+        return null;
     }
 
     @Override
--- src/main/java/com/team/leaf/user/account/jwt/PrincipalDetailsService.java
+++ src/main/java/com/team/leaf/user/account/jwt/PrincipalDetailsService.java
@@ -1,7 +1,9 @@
 package com.team.leaf.user.account.jwt;
 
 import com.team.leaf.user.account.entity.AccountDetail;
+import com.team.leaf.user.account.entity.OAuth2Account;
 import com.team.leaf.user.account.repository.AccountRepository;
+import com.team.leaf.user.account.repository.OAuth2Repository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,34 +15,25 @@ import org.springframework.stereotype.Service;
 public class PrincipalDetailsService implements UserDetailsService {
 
     public final AccountRepository accountRepository;
-
-    /*@Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
-        AccountDetail accountDetail = accountRepository.findByEmail(email).orElseThrow(
-                () -> new RuntimeException("Not found User")
-        );
-
-        PrincipalDetails userDetails = new PrincipalDetails();
-        userDetails.setAccountDetail(accountDetail);
-
-        return userDetails;
-    }*/
+    private final OAuth2Repository oAuth2Repository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        System.out.println("Attempting to load user by username: " + email);
 
-        AccountDetail accountDetail = accountRepository.findByEmail(email).orElseThrow(
-                () -> new UsernameNotFoundException("Not found User")
-        );
-
-        System.out.println("User loaded successfully: " + accountDetail.getEmail());
+        AccountDetail accountDetail = accountRepository.findByEmail(email).orElse(null);
+        OAuth2Account oAuth2Account = oAuth2Repository.findByEmail(email).orElse(null);
 
         PrincipalDetails userDetails = new PrincipalDetails();
-        userDetails.setAccountDetail(accountDetail);
 
-        return userDetails;
+        if(accountDetail != null) {
+            userDetails.setAccountDetail(accountDetail);
+            return userDetails;
+        } else if (oAuth2Account != null) {
+            userDetails.setOAuth2Account(oAuth2Account);
+            return userDetails;
+        }
+
+        throw new UsernameNotFoundException("Not fount User");
     }
 
 }
