9eb2b1ea5f59127fbcd875d1134f7487ee8ab02d
hershyYJ <juj0478@naver.com>
2024-03-18 19:58:11 +0900

    fix: refresh token 만료 시간 필드 추가

--- src/main/java/com/team/leaf/user/account/dto/response/TokenDto.java
+++ src/main/java/com/team/leaf/user/account/dto/response/TokenDto.java
@@ -1,18 +1,17 @@
 package com.team.leaf.user.account.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class TokenDto {
 
     private String accessToken;
     private String refreshToken;
-
-    public TokenDto(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
+    private Long refreshTokenExpirationTime;
 
 }
